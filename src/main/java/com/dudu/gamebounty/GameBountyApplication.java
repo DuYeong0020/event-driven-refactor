5084dfff23b092b27760d2e17250f0356e5d2308
duyeong0020 <duyeong0020@gmail.com>
2025-12-14 00:22:23 +0900

    first commit

---
+++ src/main/java/com/dudu/gamebounty/GameBountyApplication.java
@@ -0,0 +1,13 @@
+package com.dudu.gamebounty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBountyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameBountyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dudu/gamebounty/GameBountyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudu.gamebounty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBountyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
